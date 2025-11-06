8f24abe5348aa46b3dbc45bd0faa450da8dc3410
HabibMebdoua <techameur@gmail.com>
2025-11-06 13:49:52 +0100

    fix course upload

--- src/courses_market/models.py
+++ src/courses_market/models.py
@@ -16,7 +16,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
     title = models.CharField(max_length=200)
     video = models.FileField(upload_to='courses/videos/')
-    order = models.PositiveIntegerField()
+    order = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return f"{self.course.title} - {self.title}"
